4aba7640229f94120b1a9cd06ff614a8d63b6dc3
dudoca7work <eduardajhenniffer125@gmail.com>
2024-10-14 15:08:58 -0300

    views usuario

--- banco_dados/views.py
+++ banco_dados/views.py
@@ -1,33 +1,45 @@
-#from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+# seu_app/views.py
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Usuario
-from .models import Inscricao
-from django.template import loader
-from django.shortcuts import render
+from .forms import UsuarioForm
 
-def index(request):
-    return HttpResponse("dudoca")
+# View para listar usuários
+def usuario_list(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'usuarios/usuario_list.html', {'usuarios': usuarios})
 
-def usuários(request):
-    Usuario_list = Usuario.objects.all()
-    template = loader.get_template ("banco_dados/usuários.html")
-    context = {
-        "Usuario_list" : Usuario_list
-    }
-    #return render(request, "banco_dados/usuários.html", context)
-    return HttpResponse(template.render(context,request))
+# View para detalhes do usuário
+def usuario_detail(request, pk):
+    usuario = get_object_or_404(Usuario, pk=pk)
+    return render(request, 'usuarios/usuario_detail.html', {'usuario': usuario})
 
-# Leave the rest of the views (usuários, results, vote) unchanged
+# View para criar usuário
+def usuario_create(request):
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('usuario_list')
+    else:
+        form = UsuarioForm()
+    return render(request, 'usuarios/usuario_form.html', {'form': form})
 
-def details(request):
-    Inscricao_list = Inscricao.objects.all()
-    template = loader.get_template ("banco_dados/details.html")
-    context = {
-        "Inscricao_list" : Inscricao_list
-    }
-    #return render(request, "banco_dados/details.html", context)
-    return HttpResponse(template.render(context,request))
+# View para editar usuário
+def usuario_update(request, pk):
+    usuario = get_object_or_404(Usuario, pk=pk)
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST, instance=usuario)
+        if form.is_valid():
+            form.save()
+            return redirect('usuario_detail', pk=pk)
+    else:
+        form = UsuarioForm(instance=usuario)
+    return render(request, 'usuarios/usuario_form.html', {'form': form})
 
-# Leave the rest of the views (details, results, vote) unchanged
+# View para deletar usuário
+def usuario_delete(request, pk):
+    usuario = get_object_or_404(Usuario, pk=pk)
+    if request.method == 'POST':
+        usuario.delete()
+        return redirect('usuario_list')
+    return render(request, 'usuarios/usuario_confirm_delete.html', {'usuario': usuario})
